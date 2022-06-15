98a72b70359bf2a9f4788fe5476fc57ac6af6076
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-15 22:21:14 +0300

    Stage 1/5: Unfair computer

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -1,1 +1,12 @@
 # Write your code here
+user_choice = input()
+msg = 'Sorry, but the computer chose'
+computer_choice = None
+if user_choice == "rock":
+    computer_choice = "paper"
+elif user_choice == "paper":
+    computer_choice = "scissors"
+elif user_choice == "scissors":
+    computer_choice = "rock"
+
+print(f'{msg} {computer_choice}')
